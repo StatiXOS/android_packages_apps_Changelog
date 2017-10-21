441dcbceb6bc5c7cafbf9d9fb947290f72a7bf9a
ByteHamster <info@bytehamster.com>
2017-10-21 16:01:45 +0200

    PreferenceCompat

--- app/src/main/java/com/bytehamster/changelog/Preferences.java
+++ app/src/main/java/com/bytehamster/changelog/Preferences.java
@@ -1,116 +1,16 @@
 package com.bytehamster.changelog;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.Preference.OnPreferenceClickListener;
-import android.preference.PreferenceManager;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class Preferences extends PreferenceActivity {
-    @SuppressWarnings("deprecation")
+public class Preferences extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.prefs);
+        setContentView(R.layout.activity_settings);
 
-        if(getActionBar() != null) getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(Preferences.this);
-
-        findPreference("server_url").setSummary(prefs.getString("server_url", Main.DEFAULT_GERRIT_URL));
-        findPreference("server_url").setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(Preferences.this);
-                alert.setTitle(R.string.server_url);
-                final View urlDialogView = View.inflate(getBaseContext(), R.layout.dialog_select_url, null);
-                final EditText editText = urlDialogView.findViewById(R.id.gerrit_url_edit);
-                final RadioButton radioCustom = urlDialogView.findViewById(R.id.gerrit_button_custom);
-                final RadioButton radioOmnirom = urlDialogView.findViewById(R.id.gerrit_button_omnirom);
-                final RadioButton radioLineageos = urlDialogView.findViewById(R.id.gerrit_button_lineageos);
-
-                String url = prefs.getString("server_url", Main.DEFAULT_GERRIT_URL);
-
-                if (url.equals(getString(R.string.gerrit_url_lineageos))) {
-                    radioLineageos.setChecked(true);
-                } else if (url.equals(getString(R.string.gerrit_url_omnirom))) {
-                    radioOmnirom.setChecked(true);
-                } else {
-                    radioCustom.setChecked(true);
-                    editText.setVisibility(View.VISIBLE);
-                    editText.setText(url);
-                }
-
-                radioCustom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                        editText.setVisibility(radioCustom.isChecked() ? View.VISIBLE : View.GONE);
-                        editText.setText(prefs.getString("server_url", Main.DEFAULT_GERRIT_URL));
-                    }
-                });
-                alert.setView(urlDialogView);
-                alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String serverUrl;
-                        if (radioOmnirom.isChecked()) {
-                            serverUrl = getString(R.string.gerrit_url_omnirom);
-                        } else if (radioLineageos.isChecked()) {
-                            serverUrl = getString(R.string.gerrit_url_lineageos);
-                        } else {
-                            serverUrl = editText.getText().toString();
-                        }
-                        prefs.edit().putString("server_url", serverUrl).apply();
-
-                        findPreference("server_url").setSummary(serverUrl);
-                        clearCache();
-                    }
-                });
-                alert.setNegativeButton(android.R.string.cancel, null);
-                alert.show();
-                return true;
-            }
-        });
-
-        findPreference("clear_cache").setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                clearCache();
-                return false;
-            }
-        });
-
-        findPreference("about").setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(Preferences.this);
-                alert.setTitle(R.string.about);
-                alert.setMessage(Html.fromHtml(getString(R.string.about_message)));
-                alert.setPositiveButton(android.R.string.ok, null);
-                Dialog d = alert.show();
-                ((TextView) d.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
-                return true;
-            }
-        });
-    }
-
-    private void clearCache() {
-        Toast.makeText(Preferences.this, R.string.cleared_cache, Toast.LENGTH_LONG).show();
-        ChangeCacheDatabase database = new ChangeCacheDatabase(Preferences.this);
-        database.clearCache();
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(Preferences.this);
-        prefs.edit().putLong("cache_lastrefresh", 0).apply();
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
