5dc9fba94c3e92c29bb78d057a07f87a4d1e3cd1
ByteHamster <info@bytehamster.com>
2018-01-07 19:17:52 +0100

    Added Redmi Note 4

--- device-definition-converter.py
+++ device-definition-converter.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
     print('Need exactly one argument.')
     sys.exit(0)
 
-file_content = urllib.request.urlopen("https://raw.githubusercontent.com/omnirom/"+sys.argv[1]+"/master/omni.dependencies")
+file_content = urllib.request.urlopen("https://raw.githubusercontent.com/omnirom/"+sys.argv[1]+"/HEAD/omni.dependencies")
 json_object = json.load(file_content)
 
 print("\n\n<device>\n	<name></name>\n	<code></code>\n	<repos>")
