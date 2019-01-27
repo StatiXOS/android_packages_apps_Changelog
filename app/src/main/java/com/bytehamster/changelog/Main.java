fad4897825cb0b1e06b95bc23d6226e4073c7a11
Anay Wadhera <anay1018@gmail.com>
2019-01-26 17:34:23 -0800

    Changelog: electrify

--- app/src/main/java/com/bytehamster/changelog/Main.java
+++ app/src/main/java/com/bytehamster/changelog/Main.java
@@ -59,7 +59,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Main extends Activity {
 
-    public static final String DEFAULT_GERRIT_URL = "https://gerrit.dirtyunicorns.com/";
+    public static final String DEFAULT_GERRIT_URL = "https://review.statixos.me/";
     public static final String DEFAULT_BRANCH = "";
     public static final int MAX_CHANGES = 2000;
     public static final int MAX_CHANGES_FETCH = 2000;
